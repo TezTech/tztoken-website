24f58fe14e169fab683a1d4356039de6f572d5e3
Stephen Andrews <stephen@andrewstribe.co.nz>
2019-07-23 10:59:34 +1200

    Adding ability to view balances

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/fi");
 editor.session.setOption("useWorker", false);
 function tokenTable(d){
-    d = eztz.utility.mic2arr(d);
-    $("#tokenDtails").show();
-    $("#dis_name").html(d[1]);
-    $("#dis_symbol").html(d[2]);
-    $("#dis_decimals").html((d[3].toString().length-1));
-    $("#tokentable>tbody").empty();
-    for(var i = 0; i < d[0].keys.length; i++){      
-      $("#tokentable tbody").append('<tr><td style="text-align:center;">'+d[0].keys[i]+'</td><td style="text-align:center;">'+(d[0].vals[i]/d[3]).toFixed((d[3].toString().length-1))+d[2]+'</td></tr> ');
-    }
+		var address = $('#address').val();
+		var contract = $('#contract').val();
+		d = eztz.utility.mic2arr(d);
+		eztz.contract.storage(contract).then(function(dd){
+			dd = eztz.utility.mic2arr(dd);
+			var dps = dd[1][2];
+			var tn = dd[1][1];
+			$("#tokenDtails").show();
+			$("#tokentable>tbody").empty();
+			$("#tokentable tbody").append('<tr><td style="text-align:center;">'+(d/Math.pow(10, dps)).toFixed(dps)+" "+tn+'</td></tr>');
+			$('button#observe').attr("disabled", false)
+			$('button#observe').html("Check Balance")
+		});
 }
 copyToClipboard = function(text) {
 if (window.clipboardData && window.clipboardData.setData) {
@@ -48,9 +52,17 @@ $('document').ready(function(){
     $('button#observe').click(function(e){
       e.preventDefault();
       var contract = $('#contract').val();
+      var address = $('#address').val();
       $('#trans_contract').val(contract);
-      eztz.contract.storage(contract).then(tokenTable)
-      .catch(function(e){alert("There was an error getting your token contract");});
+      $('#trans_from').val(address);
+			$('button#observe').attr("disabled", true)
+			$('button#observe').html("Loading...")
+			
+			tztoken.checkBalance(contract, address).then(tokenTable).catch(function(e){
+				alert("There was an error getting your token contract");
+				$('button#observe').attr("disabled", false)
+				$('button#observe').html("Check Balance")
+			});
     });
     $('button#transfer').click(function(e){
       var contract = $('#trans_contract').val(),
@@ -99,7 +111,7 @@ $('document').ready(function(){
       totalsupply = totalsupply * Math.pow(10, decimals);
       $('#compiled').html(`./tezos-client originate contract MyToken for `+tz
       +` transferring 0 from `+tz
-      +` running 'parameter bytes;storage (pair (map address mutez) (pair string (pair string nat)));code{DUP;CDR;NIL operation;PAIR;SWAP;CAR;DUP;PUSH nat 4;PUSH nat 0;SLICE;IF_NONE{PUSH nat 100;FAILWITH}{};DUP;PUSH bytes 0x19308cc0;COMPARE;EQ;IF{DROP;DUP;SIZE;PUSH nat 4;SWAP;SUB;DUP;GT;IF{}{PUSH nat 102;FAILWITH};ABS;PUSH nat 4;SLICE;IF_NONE{PUSH nat 101;FAILWITH}{};UNPACK (pair address (pair mutez (option bytes)));IF_NONE{PUSH nat 103;FAILWITH}{};PAIR;NONE mutez;PAIR;SENDER;DIP{DUP;CDDDAR};MEM;DIP{PUSH bool True};COMPARE;EQ;IF{}{PUSH string "Failed assert";FAILWITH};SENDER;DIP{DUP;CDDDAR};GET;IF_NONE{PUSH string "Key not found in map";FAILWITH}{};SWAP;SET_CAR;DUP;CAR;DIP{DUP;CDADAR};COMPARE;GE;IF{}{PUSH string "Failed assert";FAILWITH};SENDER;DIP{DUP;CAR;DIP{DUP;CDADAR};SUB;SOME};DIIP{DUP;CDDDAR};UPDATE;SWAP;SET_CDDDAR;PUSH mutez 0;SWAP;SET_CAR;DUP;CDAAR;DIP{DUP;CDDDAR};MEM;DIP{PUSH bool True};COMPARE;EQ;IF{DUP;CDAAR;DIP{DUP;CDDDAR};GET;IF_NONE{PUSH string "Key not found in map";FAILWITH}{};SWAP;SET_CAR}{};DUP;CDAAR;DIP{DUP;CAR;DIP{DUP;CDADAR};ADD;SOME};DIIP{DUP;CDDDAR};UPDATE;SWAP;SET_CDDDAR;DUP;CDADDR;IF_NONE{PUSH bool False}{DROP;PUSH bool True};DIP{PUSH bool True};COMPARE;EQ;IF{DUP;CDADDR;IF_NONE{PUSH string "Optional value is empty";FAILWITH}{};DIP{DUP;CDADAR};SWAP;PAIR;DIP{SENDER};SWAP;PAIR;DIP{PUSH mutez 0};DIIP{DUP;CDAAR;CONTRACT (pair address (pair mutez bytes));IF_NONE{PUSH string "Invalid contract";FAILWITH}{}};TRANSFER_TOKENS;DIP{DUP;CDDAR};CONS;SWAP;SET_CDDAR}{};CDDR}{DROP;PUSH nat 400;FAILWITH}}' --init '(Pair {Elt "`+tz
+      +` running 'parameter bytes;storage (pair (big_map address mutez) (pair string (pair string nat)));code{DUP;CDR;NIL operation;PAIR;SWAP;CAR;DUP;PUSH nat 4;PUSH nat 0;SLICE;IF_NONE{PUSH nat 100;FAILWITH}{};DUP;PUSH bytes 0x19308cc0;COMPARE;EQ;IF{DROP;DUP;SIZE;PUSH nat 4;SWAP;SUB;DUP;GT;IF{}{PUSH nat 102;FAILWITH};ABS;PUSH nat 4;SLICE;IF_NONE{PUSH nat 101;FAILWITH}{};UNPACK (pair address (pair mutez (option bytes)));IF_NONE{PUSH nat 103;FAILWITH}{};PAIR;NONE mutez;PAIR;SENDER;DIP{DUP;CDDDAR};MEM;DIP{PUSH bool True};COMPARE;EQ;IF{}{PUSH string "Failed assert";FAILWITH};SENDER;DIP{DUP;CDDDAR};GET;IF_NONE{PUSH string "Key not found in map";FAILWITH}{};SWAP;SET_CAR;DUP;CAR;DIP{DUP;CDADAR};COMPARE;GE;IF{}{PUSH string "Failed assert";FAILWITH};SENDER;DIP{DUP;CAR;DIP{DUP;CDADAR};SUB;SOME};DIIP{DUP;CDDDAR};UPDATE;SWAP;SET_CDDDAR;PUSH mutez 0;SWAP;SET_CAR;DUP;CDAAR;DIP{DUP;CDDDAR};MEM;DIP{PUSH bool True};COMPARE;EQ;IF{DUP;CDAAR;DIP{DUP;CDDDAR};GET;IF_NONE{PUSH string "Key not found in map";FAILWITH}{};SWAP;SET_CAR}{};DUP;CDAAR;DIP{DUP;CAR;DIP{DUP;CDADAR};ADD;SOME};DIIP{DUP;CDDDAR};UPDATE;SWAP;SET_CDDDAR;DUP;CDADDR;IF_NONE{PUSH bool False}{DROP;PUSH bool True};DIP{PUSH bool True};COMPARE;EQ;IF{DUP;CDADDR;IF_NONE{PUSH string "Optional value is empty";FAILWITH}{};DIP{DUP;CDADAR};SWAP;PAIR;DIP{SENDER};SWAP;PAIR;DIP{PUSH mutez 0};DIIP{DUP;CDAAR;CONTRACT (pair address (pair mutez bytes));IF_NONE{PUSH string "Invalid contract";FAILWITH}{}};TRANSFER_TOKENS;DIP{DUP;CDDAR};CONS;SWAP;SET_CDDAR}{};CDDR}{DROP;PUSH nat 400;FAILWITH}}' --init '(Pair {Elt "`+tz
       +`" `+ totalsupply.toString()
       +`} (Pair "`+ name
       +`" (Pair "`+ symbol
